2b1e9b4577c8d52d523901d1dd1e2c46990e3935
snchr <sanchirenkhamgalan@gmail.com>
2023-06-16 21:41:35 +0800

    ylagchaa todruulah dutuu

--- app.js
+++ app.js
@@ -25,17 +25,49 @@ document.querySelector(".btn-roll").addEventListener("click", function(){
    
     if ( diceNumber !== 1 ){
         roundScore += diceNumber;
-        console.log(activePlayer);
         document.getElementById("current-" + activePlayer).textContent = roundScore;
     } else {
         document.getElementById('current-' + activePlayer).textContent = 0;
         activePlayer === 0 ? ( activePlayer = 1 ) : ( activePlayer = 0 );
         document.querySelector('.player-0-panel').classList.toggle("active");
         document.querySelector('.player-1-panel').classList.toggle("active");
-        
-        
         roundScore = 0;
+        diceDom.style.display = "none";
 
     }
 
+ });
+
+  document.querySelector(".btn-hold").addEventListener("click", function(){
+      scores[ activePlayer ] += roundScore;
+     
+      document.getElementById('score-' + activePlayer).textContent = scores[ activePlayer];
+      
+      roundScore = 0;
+      document.getElementById("current-" + activePlayer).textContent = 0;
+      activePlayer === 0 ? ( activePlayer = 1 ) : ( activePlayer = 0 );
+      document.querySelector('.player-0-panel').classList.toggle("active");
+      document.querySelector('.player-1-panel').classList.toggle("active");
+      diceDom.style.display = "none";
 });
+
+document.querySelector(".btn-new").addEventListener("click", function(){
+    activePlayer = 0;
+
+    scores = [0, 0];
+    scores[0] = 0; 
+    scores[1] = 0; 
+
+    roundScore = 0;
+
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0; 
+    activePlayer === 0 ? ( activePlayer = 1 ) : ( activePlayer = 0 );
+    document.querySelector('.player-0-panel').classList.toggle("active");
+    document.querySelector('.player-1-panel').classList.toggle("active");
+      
+
+    diceDom.style.display = "none"; 
+})
